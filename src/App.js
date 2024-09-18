f32da3e082a77324b4db517dc358e03a297615e2
unknown <mohdshereefn@gmail.com>
2024-09-18 18:12:39 +0530

    set frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { pdfjs } from 'react-pdf';
+import PdfComp from './components/PdfComp';
+import { FaBars, FaTrash } from 'react-icons/fa';
+import { toast, Toaster } from 'react-hot-toast';
+
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [file, setFile] = useState('');
+  const [allImage, setAllImage] = useState(null);
+  const [pdfFile, setPdfFile] = useState(null);
+
+  useEffect(() => {
+    getPdf();
+  }, []);
+
+  const getPdf = async () => {
+    const result = await axios.get('http://localhost:5000/get-files');
+    console.log(result.data.data);
+    setAllImage(result.data.data);
+  };
+
+  const submitImage = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('file', file);
+
+    const result = await axios.post('http://localhost:5000/upload-files', formData, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+      withCredentials: true
+    });
+    if (result.data.status === 'OK') {
+      toast.success('Uploaded successfully!');
+      getPdf();
+    }
+  };
+
+  const showPdf = (pdf) => {
+    setPdfFile(`http://localhost:5000/files/${pdf}`);
+  };
+
+  const deletePdf = async (pdfId) => {
+    try {
+      await axios.delete(`http://localhost:5000/delete-file/${pdfId}`);
+      getPdf();
+      toast.success('Deleted successfully!');
+    } catch (error) {
+      toast.error('Error deleting PDF!');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:"red"}}>
+      <Toaster position="top-right" reverseOrder={false} />
+  <nav className="navbar navbar-light" style={{ height: '50px', width: '100%' }}>
+  <img 
+      src="/signature.png" 
+      alt="Logo"
+      style={{ height: '80px', width: 'auto' }} 
+    />
+    <div style={{ display: 'flex', alignItems: 'center', marginRight: '10px' }}>
+    <FaBars style={{ fontSize: '20px', color: 'black' }} />
+    <FaBars style={{ fontSize: '25px', color: 'red' }} />
+        </div>
+</nav>
+
+    <div className="container">
+      <div className="left-section">
+        <form className="formStyle p-4 shadow-lg  bg-light" onSubmit={submitImage}>
+  <h4 className="text-center fw-bold text-uppercase mb-4 text-danger">Upload Your PDF</h4>
+
+  <div className="mb-3">
+    <label htmlFor="title" className="form-label fw-bold">
+      Title
+    </label>
+    <input
+      type="text"
+      className="form-control"
+      id="title"
+      placeholder="Enter PDF Title"
+      required
+      onChange={(e) => setTitle(e.target.value)}
+    />
+  </div>
+
+  <div className="mb-3">
+    <label htmlFor="fileUpload" className="form-label fw-bold">
+      Choose PDF File
+    </label>
+    <input
+      type="file"
+      className="form-control"
+      id="fileUpload"
+      accept="application/pdf"
+      required
+      onChange={(e) => setFile(e.target.files[0])}
+    />
+  </div>
+
+  <div className="d-grid gap-2">
+    <button className="btn btn-danger btn-block fw-bold" type="submit" style={{borderRadius:"30px"}}>
+      <i className="bi bi-upload"></i> SUBMIT
+    </button>
+  </div>
+</form>
+
+
+        <div className="uploaded">
+          <h5 style={{color:"white"}}>UPLOADED PDFs</h5>
+          <div className="output-div">
+            {allImage == null
+              ? ''
+              : allImage.map((data) => {
+                  return (
+                    <div className="inner-div" key={data._id}>
+                      <h6>Title: {data.title}</h6>
+                      <button className="btn btn-dark" onClick={() => showPdf(data.pdf)} style={{ marginRight: '5px' }}>
+                        Show PDF
+                      </button>
+                      <button className="btn btn-light" onClick={() => deletePdf(data._id)}>
+                        <FaTrash /> 
+                      </button>
+                    </div>
+                  );
+                })}
+          </div>
+        </div>
+      </div>
+
+      <div className="right-section">
+        {pdfFile ? (
+          <PdfComp pdfFile={pdfFile} />
+        ) : (
+          <div className="empty-message-container">
+      <p>No PDF selected.</p>
+      <p>Please select a PDF from the uploaded list to view and select.</p>
+      <img
+        src="/vecteezy_3d-cute-kid-character-confused-on-a-laptop_34618258.png" 
+        alt="No PDF"
+        style={{ width: '200px', height: 'auto', marginTop: '20px' }}
+      />
+    </div>
+        )}
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/components/PdfComp.js
@@ -0,0 +1,119 @@
+
+import { useState, useRef, useEffect } from "react";
+import { Document, Page } from "react-pdf";
+import axios from 'axios';
+import { FaArrowUp } from 'react-icons/fa'; 
+
+function PdfComp(props) {
+  const [numPages, setNumPages] = useState(0);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [selectedPages, setSelectedPages] = useState([]);
+  const containerRef = useRef(null);
+  const [showScrollToTop, setShowScrollToTop] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (containerRef.current.scrollTop > 300) {
+        setShowScrollToTop(true);
+      } else {
+        setShowScrollToTop(false);
+      }
+    };
+
+    const container = containerRef.current;
+    container.addEventListener('scroll', handleScroll);
+
+    return () => {
+      container.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  const handlePageSelection = (pageNumber) => {
+    if (selectedPages.includes(pageNumber)) {
+      setSelectedPages(selectedPages.filter(page => page !== pageNumber));
+    } else {
+      setSelectedPages([...selectedPages, pageNumber]);
+    }
+  };
+
+  const handleDownloadSelectedPages = async () => {
+    if (selectedPages.length > 0) {
+        try {
+            const response = await axios.post(
+                'http://localhost:5000/download-selected-pages',
+                {
+                    pdfFile: props.pdfFile.split('/').pop(),
+                    selectedPages: selectedPages
+                },
+                { responseType: 'blob' } 
+            );
+
+            
+            const url = window.URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' }));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'selected_pages.pdf');
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+
+            window.URL.revokeObjectURL(url);
+
+        } catch (error) {
+            console.error("Error downloading selected pages:", error);
+        }
+    } else {
+        alert("No pages selected");
+    }
+  };
+
+  const scrollToTop = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className="pdf-div" ref={containerRef}>
+      <div className="instructions-container">
+        <div className="instructions">Select pages to download</div>
+        <button className="fixed-button" onClick={handleDownloadSelectedPages} style={{borderRadius:'20px'}}>
+          Download Selected Pages
+        </button>
+      </div>
+      <p>Page {pageNumber} of {numPages}</p>
+      <Document file={props.pdfFile} onLoadSuccess={onDocumentLoadSuccess}>
+        {Array.from(new Array(numPages), (x, i) => i + 1).map((page) => (
+         <div key={page} className="page-wrapper">
+           <div className="page-container">
+             <Page
+               pageNumber={page}
+               renderTextLayer={false}
+               renderAnnotationLayer={false}
+             />
+             <div className="checkbox-wrapper">
+               <input
+                 type="checkbox"
+                 checked={selectedPages.includes(page)}
+                 onChange={() => handlePageSelection(page)}
+               />
+             </div>
+           </div>
+         </div>
+        ))}
+      </Document>
+      {showScrollToTop && (
+        <button className="scroll-to-top-button" onClick={scrollToTop}>
+          <FaArrowUp />
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default PdfComp;
+
